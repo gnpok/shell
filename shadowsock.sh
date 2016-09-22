31e67daa7d0b5f24fbb90201a0aaaafe68427230
gnp <742926717@qq.com>
2016-09-22 09:18:47 +0800

    修改配置

--- shadowsock.sh
+++ shadowsock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup sslocal -s 45.78.46.185 -p 443  -l 1080 -k ODc1NDBkNG -t 600 -m aes-256-cfb >/dev/null 2>&1 &
+sslocal -s 23.106.158.90 -p 443  -l 1080 -k 密码 -t 600 -m aes-256-cfb >/dev/null 2>&1 
