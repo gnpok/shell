afa593ad93f6555963062cad53375d32f9eba8fb
hodor <742926717@qq.com>
2016-12-16 10:08:59 +0800

    添加针对mysql的shell脚本

--- mysqlOperation/webService_isRunning.sh
+++ mysqlOperation/webService_isRunning.sh
@@ -7,3 +7,25 @@ for S in "${soft[@]}";do
 		`service $S start` 
 	fi 
 done
+
+#只检测mysql是否在运行建议使用下面
+result=`/usr/bin/mysqladminping`
+expected='mysqld is alive'
+
+if[[ "$result"!= "$expected"]]
+then
+echo"It's dead - restart mysql"
+    
+# email subject
+SUBJECT="[MYSQL ERROR] - Attempting to restart service"    
+# Email To ?
+EMAIL="info@endyourif.com"    
+# Email text/message
+EMAILMESSAGE="/tmp/emailmessage.txt"
+echo"$result was received"> $EMAILMESSAGE
+echo"when we were expected $expected">>$EMAILMESSAGE
+# send an email using /bin/mail
+mail -s "$SUBJECT""$EMAIL"< $EMAILMESSAGE
+   
+sudo/etc/init.d/mysqlrestart
+fi
