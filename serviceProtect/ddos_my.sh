bb0ca3bd307731b449c44de1ee6a30a2a269ec51
hodor <742926717@qq.com>
2017-03-13 23:34:30 +0800

    Update ddos_my.sh
    添加另一种获取管道命令

--- serviceProtect/ddos_my.sh
+++ serviceProtect/ddos_my.sh
@@ -1,4 +1,5 @@
 #!/bin.bash
+#status=`netstat -na|awk '$5 ~ /[0-9]+:[0-9]+/ {print $5}'|awk -F ":" -- '{print $1}' |sort -n|uniq -c |sort -n|tail -n 1|grep -v 127.0.0.1`
 status=`netstat -anlp|grep 80|grep tcp|awk '{print $5}'|awk -F: '{print $1}'|sort|uniq -c|sort -nr|head -n20 | netstat -ant |awk '/:80/{split($5,ip,":");++A[ip[1]]}END{for(i in A) print A[i],i}' |sort -rn|head -n 1`
 NUM=`echo $status|awk '{print $1}'`
 IP=`echo $status|awk '{print $2}'`
